3b3d27da8a0cb3f59e2152f5d489b4eef4052269
Dylan Wu <dywu@microsoft.com>
2023-06-18 09:22:40 -0700

    add working sample separated cli

--- test/remedationsHardcoded.sh
+++ test/remedationsHardcoded.sh
@@ -10,4 +10,4 @@ az account set --subscription "f67cc918-f64f-4c3f-aa24-a855465f9d41"
 # Get the credentials for the AKS cluster
 az aks get-credentials --resource-group "rg-remedations" --name "aks-remedations-test"
 
-az aks update --resource-group "rg-remedations" --name "aks-remedations-test" --tags RemediationsPOCUpdate=true
+az aks update --resource-group "rg-remedations" --name "aks-remedations-test" --tags "RemediationsPOCUpdate=true"
