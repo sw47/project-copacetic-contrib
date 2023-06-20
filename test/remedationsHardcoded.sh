836cca2a35098a1dc4932c80e4325be67dea3822
Dylan Wu <dywu@microsoft.com>
2023-06-20 10:22:22 -0700

    Updates to bash files

--- test/remedationsHardcoded.sh
+++ test/remedationsHardcoded.sh
@@ -10,6 +10,7 @@ body="{\"definition\": {\"id\": $PipelineId}}"
 # Invoke the REST API
 pat=$(az keyvault secret show --id "https://remediationskv.vault.azure.net/secrets/DylanPat" --query "value" -o "tsv")
 az rest --method post --uri "https://dev.azure.com/msazure/one/_apis/build/builds?api-version=6.1-preview.6" --body "$body" --headers "Content-Type=application/json" "Authorization=Basic $(echo -n ":$pat" | base64)"
+
 # Invoke the REST API
 
 # Assign the passed in arguments
@@ -19,6 +20,17 @@ az rest --method post --uri "https://dev.azure.com/msazure/one/_apis/build/build
 
 
 # # Get the credentials for the AKS cluster
-# az aks get-credentials --resource-group "rg-remedations" --name "aks-remedations-test"
+az aks get-credentials --resource-group "rg-remedations" --name "aks-remedations-test"
+
+#!/bin/bash
+
+# Set your parameters here
+ACR_NAME=crremediations
+IMAGE_NAME=nginx
+
+# Get the latest tag
+#LATEST_TAG=$(az acr repository show-tags --name $ACR_NAME --repository $IMAGE_NAME --orderby time_desc --output json | jq -r '.[0]')
+az acr repository show-tags --name $ACR_NAME --repository $IMAGE_NAME --orderby time_desc
+#echo $LATEST_TAG
 
 # az aks update --resource-group "rg-remedations" --name "aks-remedations-test" --tags "RemediationsPOCUpdate=true"
