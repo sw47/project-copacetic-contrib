7242620856661854e0943473fc3f3cbf8d6c4e61
Dylan Wu <dywu@microsoft.com>
2023-06-20 11:04:23 -0700

    1) Update to retrieve latest tagged image from ACR 2) Add 5 minute wait

--- test/remedationsHardcoded.sh
+++ test/remedationsHardcoded.sh
@@ -6,10 +6,10 @@ az login
 az account set --subscription "f67cc918-f64f-4c3f-aa24-a855465f9d41"
 
 # Define the pipeline parameters
-body="{\"definition\": {\"id\": $PipelineId}}"
+#body="{\"definition\": {\"id\": $PipelineId}}"
 # Invoke the REST API
-pat=$(az keyvault secret show --id "https://remediationskv.vault.azure.net/secrets/DylanPat" --query "value" -o "tsv")
-az rest --method post --uri "https://dev.azure.com/msazure/one/_apis/build/builds?api-version=6.1-preview.6" --body "$body" --headers "Content-Type=application/json" "Authorization=Basic $(echo -n ":$pat" | base64)"
+#pat=$(az keyvault secret show --id "https://remediationskv.vault.azure.net/secrets/DylanPat" --query "value" -o "tsv")
+#az rest --method post --uri "https://dev.azure.com/msazure/one/_apis/build/builds?api-version=6.1-preview.6" --body "$body" --headers "Content-Type=application/json" "Authorization=Basic $(echo -n ":$pat" | base64)"
 
 # Invoke the REST API
 
@@ -29,8 +29,10 @@ ACR_NAME=crremediations
 IMAGE_NAME=nginx
 
 # Get the latest tag
-#LATEST_TAG=$(az acr repository show-tags --name $ACR_NAME --repository $IMAGE_NAME --orderby time_desc --output json | jq -r '.[0]')
-az acr repository show-tags --name $ACR_NAME --repository $IMAGE_NAME --orderby time_desc
+LATEST_TAG=$(az acr repository show-tags --name $ACR_NAME --repository $IMAGE_NAME --orderby time_desc --top 1 -o "tsv")
+echo "crremediations.azurecr.io/nginx:$LATEST_TAG"
+#kubectl set image deployment/nginx-deployment nginx=crremediations.azurecr.io/nginx:$LATEST_TAG
+
 #echo $LATEST_TAG
 
 # az aks update --resource-group "rg-remedations" --name "aks-remedations-test" --tags "RemediationsPOCUpdate=true"
--- test/remediations-poc-final.sh
+++ test/remediations-poc-final.sh
@@ -3,6 +3,9 @@ body="{\"definition\": {\"id\": $PipelineId}}"
 pat=$(az keyvault secret show --id "https://remediationskv.vault.azure.net/secrets/DylanPat" --query "value" -o "tsv")
 az rest --method post --uri "https://dev.azure.com/msazure/one/_apis/build/builds?api-version=6.1-preview.6" --body "$body" --headers "Content-Type=application/json" "Authorization=Basic $(echo -n ":$pat" | base64)"
 
+# Wait 5 minutes
+sleep 300
+
 az account set --subscription $SubscriptionId
 
 az aks get-credentials --resource-group $ResourceGroupName --name $AksClusterName
@@ -11,4 +14,9 @@ az aks update --resource-group $ResourceGroupName --name $AksClusterName --tags 
 
 az aks install-cli --only-show-errors
 
-kubectl set image deployment/nginx-deployment nginx=crremediations.azurecr.io/nginx:latest
+az aks update -n $AksClusterName -g $ResourceGroupName --attach-acr "crremediations"
+
+# Get the latest tag
+LATEST_TAG=$(az acr repository show-tags --name $ACR_NAME --repository $IMAGE_NAME --orderby time_desc --top 1 -o "tsv")
+
+kubectl set image deployment/nginx-deployment nginx=crremediations.azurecr.io/nginx:$LATEST_TAG
