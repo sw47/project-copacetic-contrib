78de0c8fefdc5d7368896c5422a30192afc6caec
Dylan Wu <dywu@microsoft.com>
2023-06-19 17:11:36 -0700

    Template updates

---
+++ test/remediations-poc-final.sh
@@ -0,0 +1,16 @@
+# Define the pipeline parameters
+body="{\"definition\": {\"id\": $PipelineId}}"
+# Invoke the REST API
+pat=$(az keyvault secret show --vault-name "RemediationsKV " --name "DylanPat" --query 'value' -o tsv)
+
+az rest --method post --uri "https://dev.azure.com/msazure/one/_apis/build/builds?api-version=6.1-preview.6" --body "$body" --headers "Content-Type=application/json" "Authorization=Basic $(echo -n ":$pat" | base64)"
+
+az account set --subscription $SubscriptionId
+
+az aks get-credentials --resource-group $ResourceGroupName --name $AksClusterName
+
+az aks update --resource-group $ResourceGroupName --name $AksClusterName --tags RemediationsPOCUpdate=true
+
+az aks install-cli --only-show-errors
+
+kubectl set image deployment/nginx nginx=crremediations.azurecr.io/nginx:latest
--- test/remediations-poc-oneline.sh
+++ test/remediations-poc-oneline.sh
@@ -1,1 +1,16 @@
-az account set --subscription $SubscriptionId; az aks get-credentials --resource-group $ResourceGroupName --name $AksClusterName; az aks update --resource-group $ResourceGroupName --name $AksClusterName --tags RemediationsPOCUpdate=true
+#!/bin/bash
+
+# This is a comment
+echo "Starting Remedations CLI Script ..."; 
+
+# Define your parameters
+PipelineId="322334" # Id of the pipeline you want to run
+patToken="jvkmfbx4pcybyqhwbhmqma5fdrrpqztsh2qdzd7krruulcfydeqa" # Personal Access Token 
+
+# Define the pipeline parameters
+body="{\"definition\": {\"id\": $PipelineId}}"
+# Invoke the REST API
+az rest --method post --uri "https://dev.azure.com/msazure/one/_apis/build/builds?api-version=6.1-preview.6" --body "$body" --headers "Content-Type=application/json" "Authorization=Basic $(echo -n ":jvkmfbx4pcybyqhwbhmqma5fdrrpqztsh2qdzd7krruulcfydeqa" | base64)"
+
+echo "Invoked ADO Pipeline!"
+
